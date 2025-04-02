5fea014bbcd6ac65ff1f4d0cb1406bf247ddd7ce
natha <108113193+Mandaiya@users.noreply.github.com>
2025-04-03 04:44:21 +0530

    Update __init__.py

--- ssnbot/__init__.py
+++ ssnbot/__init__.py
@@ -12,7 +12,7 @@ pattern = re.compile(r"^.\d+$")
 APP_ID = os.environ.get("APP_ID", "28045580")
 API_HASH = os.environ.get("API_HASH", "83001e24418ec7f54bfe95d4e390419f")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7476307432:AAESoS055Z4ixMzoiKS9x4YURy0gySKkERU")
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://sinvadev:sinvadev12345@cluster987.rz6me.mongodb.net/?retryWrites=true&w=majority&appName=Cluster987")
 OWNER_ID = int(os.environ.get('OWNER_ID', "655594746"))
 MUST_JOIN = os.environ.get("MUST_JOIN", "https://t.me/Puthusa_yosi")
 ADMINS = [
