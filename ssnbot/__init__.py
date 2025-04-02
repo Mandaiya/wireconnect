9361ed4a1cae2f28ca7c34ae1bc6eab32ec5ef7b
natha <108113193+Mandaiya@users.noreply.github.com>
2025-04-03 04:30:50 +0530

    Update __init__.py

--- ssnbot/__init__.py
+++ ssnbot/__init__.py
@@ -9,15 +9,15 @@ load_dotenv(override=True)
 pattern = re.compile(r"^.\d+$")
 
 # vars
-APP_ID = os.environ.get("APP_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+APP_ID = os.environ.get("APP_ID", "28045580")
+API_HASH = os.environ.get("API_HASH", "83001e24418ec7f54bfe95d4e390419f")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7476307432:AAESoS055Z4ixMzoiKS9x4YURy0gySKkERU")
 DB_URL = os.environ.get("DB_URL", "")
-OWNER_ID = int(os.environ.get('OWNER_ID', ""))
-MUST_JOIN = os.environ.get("MUST_JOIN", "")
+OWNER_ID = int(os.environ.get('OWNER_ID', "655594746"))
+MUST_JOIN = os.environ.get("MUST_JOIN", "https://t.me/Puthusa_yosi")
 ADMINS = [
     int(user) if pattern.search(user) else user
-    for user in os.environ.get("ADMINS", "").split()
+    for user in os.environ.get("ADMINS", "961156494").split()
 ] + [OWNER_ID]
 
 
